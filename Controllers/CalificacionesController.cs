ab256b3d086ab2f74636551ac57f4844ebba27c9
f11v <146761375+f11v@users.noreply.github.com>
2024-06-15 05:00:01 -0500

    *

---
+++ Controllers/CalificacionesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using C.Models;
+
+namespace C.Controllers
+{
+    public class CalificacionesController : Controller
+    {
+        private readonly CContext _context;
+
+        public CalificacionesController(CContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Calificaciones
+        public async Task<IActionResult> Index()
+        {
+            var cContext = _context.Calificaciones.Include(c => c.Materia).Include(c => c.Usuario);
+            return View(await cContext.ToListAsync());
+        }
+
+        // GET: Calificaciones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calificacione = await _context.Calificaciones
+                .Include(c => c.Materia)
+                .Include(c => c.Usuario)
+                .FirstOrDefaultAsync(m => m.CalificacionId == id);
+            if (calificacione == null)
+            {
+                return NotFound();
+            }
+
+            return View(calificacione);
+        }
+
+        // GET: Calificaciones/Create
+        public IActionResult Create()
+        {
+            ViewData["MateriaId"] = new SelectList(_context.Materias, "MateriaId", "MateriaId");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId");
+            return View();
+        }
+
+        // POST: Calificaciones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CalificacionId,Nota1,Nota2,Nota3,NotaFinal,NotaExtra,UsuarioId,MateriaId")] Calificacione calificacione)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(calificacione);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MateriaId"] = new SelectList(_context.Materias, "MateriaId", "MateriaId", calificacione.MateriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", calificacione.UsuarioId);
+            return View(calificacione);
+        }
+
+        // GET: Calificaciones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calificacione = await _context.Calificaciones.FindAsync(id);
+            if (calificacione == null)
+            {
+                return NotFound();
+            }
+            ViewData["MateriaId"] = new SelectList(_context.Materias, "MateriaId", "MateriaId", calificacione.MateriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", calificacione.UsuarioId);
+            return View(calificacione);
+        }
+
+        // POST: Calificaciones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CalificacionId,Nota1,Nota2,Nota3,NotaFinal,NotaExtra,UsuarioId,MateriaId")] Calificacione calificacione)
+        {
+            if (id != calificacione.CalificacionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(calificacione);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CalificacioneExists(calificacione.CalificacionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MateriaId"] = new SelectList(_context.Materias, "MateriaId", "MateriaId", calificacione.MateriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", calificacione.UsuarioId);
+            return View(calificacione);
+        }
+
+        // GET: Calificaciones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var calificacione = await _context.Calificaciones
+                .Include(c => c.Materia)
+                .Include(c => c.Usuario)
+                .FirstOrDefaultAsync(m => m.CalificacionId == id);
+            if (calificacione == null)
+            {
+                return NotFound();
+            }
+
+            return View(calificacione);
+        }
+
+        // POST: Calificaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var calificacione = await _context.Calificaciones.FindAsync(id);
+            if (calificacione != null)
+            {
+                _context.Calificaciones.Remove(calificacione);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CalificacioneExists(int id)
+        {
+            return _context.Calificaciones.Any(e => e.CalificacionId == id);
+        }
+    }
+}
